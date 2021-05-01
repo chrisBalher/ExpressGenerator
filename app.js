5bf3d095c83334ded51da84744df0579a980eec6
chrisBalher <c.baldeon.h@gmail.com>
2021-04-30 23:01:41 -0500

    Passport JWT

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,10 +16,11 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 /* Mongoose Conf */
 const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db)=>{
@@ -36,38 +38,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890'));
-
-/* Session Middleware */
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-45675',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}))
-
 app.use(passport.initialize());
-app.use(passport.session());
-
 /* Rutas Accesibles sin Autenticación */
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-/* Authentication */
-function auth (req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    next(err);
-  }
-  else {
-        next();
-  }
-}
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
